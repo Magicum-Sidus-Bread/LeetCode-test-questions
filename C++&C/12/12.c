78a4a535196e2a544ea60a8fb7f63cc87dc1ba53
Susancutie <2019300003067@whu.edu.cn>
2022-04-08 15:46:21 +0800

    12题暂未完成

---
+++ C++&C/12/12.c
@@ -0,0 +1,10 @@
+char * intToRoman(int num){
+    // char *s = calloc(16, sizeof(char));
+    int thousand = num / 1000;
+    if(thousand != 0){
+        char s1[thousand];
+        for(int i = 0;i < thousand;i++){
+
+        }
+    }
+}

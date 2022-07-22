435e397b0b0ffd536c14d9c229e4d5b2d30922a5
Susancutie <2019300003067@whu.edu.cn>
2022-07-22 21:00:40 +0800

    feature(完成27题):

---
+++ C/27/27.c
@@ -0,0 +1,25 @@
+//
+// Created by Harry Potter on 2022/7/22.
+//
+int removeElement(int* nums, int numsSize, int val){
+    if(numsSize==1 && nums[0]==val)return 0;
+    int head = 0;
+    int tail = numsSize - 1;
+    int switch_;
+    while(head < tail){
+        while(head <= tail && nums[tail]==val){
+            tail--;
+        }
+        while(head < tail && nums[head]!=val){
+            head++;
+        }
+        if(head>=0 && head<numsSize && tail>=0 && head<numsSize){
+            switch_ = nums[head];
+            nums[head] = nums[tail];
+            nums[tail] = switch_;
+        }
+    }
+    int left_num = tail + 1;
+    return left_num;
+}
+

bb47b714215d7037b138fec1d20dbf63ca2fb5ca
Susancutie <2019300003067@whu.edu.cn>
2022-08-24 16:54:23 +0800

    完成第21题

---
+++ C++/21/21.cpp
@@ -0,0 +1,43 @@
+//
+// Created by Susancutie on 2022/8/24.
+//
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *p,*q,*tail,*head;
+        head = new ListNode;
+        tail = head;
+        p = list1;
+        q = list2;
+        while(p != nullptr && q != nullptr){
+            if(p -> val <= q-> val){
+                tail -> next = p;
+                tail = p;
+                p = p -> next;
+            }
+            else {
+                tail -> next = q;
+                tail = q;
+                q = q -> next;
+            }
+        }
+        if(p == nullptr){
+            tail -> next = q;
+        }
+        if(q == nullptr){
+            tail -> next = p;
+        }
+        return head -> next;
+    }
+};

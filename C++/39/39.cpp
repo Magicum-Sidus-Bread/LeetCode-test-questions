1e2330ecc74c06dea732af301b834d8f4eed13a6
Susancutie <2019300003067@whu.edu.cn>
2022-09-13 15:46:29 +0800

    feature: 完成39题

---
+++ C++/39/39.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Susancutie on 2022/9/13.
+// Copyright © 2022 Susancutie. All rights reserved.
+//
+
+class Solution {
+public:
+    void dfs(vector<int>& candidates, int target, vector<vector<int>>& ans, vector<int>& combine, int idx){
+        if(idx == candidates.size()){
+            return;
+        }
+        if(target == 0){
+            ans.emplace_back(combine);
+            return;
+        }
+        dfs(candidates, target, ans, combine, idx + 1);
+        if(target - candidates[idx] >= 0) {
+            combine.emplace_back(candidates[idx]);
+            dfs(candidates, target - candidates[idx], ans, combine, idx);
+            combine.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> combine;
+        dfs(candidates, target, ans, combine, 0);
+        return ans;
+    }
+};
